bc6bd3b0cb03ced696d003973a98e7e19bc407d7
Niko Maroulis <nik.maroulis@gmail.com>
2026-03-21 17:23:19 -0400

    Fix Elixir version constraint to ~> 1.18

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CrowdControl.MixProject do
     [
       app: :crowd_control,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [plt_add_apps: [:mix]]
